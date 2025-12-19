9599aeadcf13e49bf69fc969829be56f73b929fe
hansen <hansen@dev.com>
2025-12-19 09:16:51 +0000

    add user schema

--- apps/api/src/modules/user/schemas/user.schema.ts
+++ apps/api/src/modules/user/schemas/user.schema.ts
@@ -1,18 +1,35 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
-
-export type UserDocument = HydratedDocument<User>;
+import { HydratedDocument, Types } from "mongoose";
+import { Phone, PhoneSchema } from "./phone.schema";
 
 @Schema()
 export class User {
     @Prop()
     uid: string;
 
-    @Prop()
+    @Prop({ type: String, minLength:2, maxLength: 30})
     name: string;
 
     @Prop()
+    avatar: string;
+
+    @Prop({ type: String, required: true})
     password: string;
+
+    @Prop(PhoneSchema)
+    phone: Phone;
+
+    @Prop({ type: String, required: false})
+    email: string;
+
+    @Prop({ type: Date})
+    createTime: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+export type UserDocumentOverride = {
+    phone: Types.Subdocument<Types.ObjectId> & Phone;
+}
+
+export type UserDocument = HydratedDocument<User, UserDocumentOverride>;
