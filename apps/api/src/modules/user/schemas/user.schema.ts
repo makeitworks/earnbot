af5dc873cd7126d4529c9f4d433e2a19d960778e
makeitworks <makeitworks@dev.co>
2025-12-27 15:11:11 +0000

    remove phone information from user schema . don't need it for now

--- apps/api/src/modules/user/schemas/user.schema.ts
+++ apps/api/src/modules/user/schemas/user.schema.ts
@@ -1,35 +1,26 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { HydratedDocument, Types } from "mongoose";
-import { Phone, PhoneSchema } from "./phone.schema";
 
 @Schema()
 export class User {
     @Prop()
     uid: string;
 
-    @Prop({ type: String, minLength:2, maxLength: 30})
+    @Prop({ type: String, minLength: 2, maxLength: 30, unique: true })
     name: string;
+
+    @Prop({ type: String, required: true })
+    email: string;
 
     @Prop()
     avatar: string;
 
-    @Prop({ type: String, required: true})
+    @Prop({ type: String, required: true })
     password: string;
 
-    @Prop(PhoneSchema)
-    phone: Phone;
-
-    @Prop({ type: String, required: false})
-    email: string;
-
-    @Prop({ type: Date})
+    @Prop({ type: Date })
     createTime: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
-
-export type UserDocumentOverride = {
-    phone: Types.Subdocument<Types.ObjectId> & Phone;
-}
-
-export type UserDocument = HydratedDocument<User, UserDocumentOverride>;
+export type UserDocument = HydratedDocument<User>;
